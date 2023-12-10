bee2742b4bdfb6627d79d810c64c61fe6a8fd161
YuCheng1122 <tommymask00@gmail.com>
2023-12-10 15:48:46 +0800

    text

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,26 @@
 const Index = () => {
   return (
     <>
-      <header className="w-full flex justify-between p-5">
-        {/* Image */}
-        <div className="min-w-[20%] bg-black"></div>
-
-        <div className="flex items-center">
-          <div className="text-gray-600 mr-2">電子週報</div>
-          <div className="text-gray-600">焦點議題</div>
+      <header className="w-full flex items-center p-5">
+        <div className="flex flex-grow space-x-2">
+          <img src="/images.png" className="w-[150px] h-[150px]"></img>
+          <img
+            src="/cq5dam.resize.390.234.png"
+            className="w-[200px] h-[150px]"
+          ></img>
+        </div>
+        <div className="ml-auto flex items-center space-x-2">
+          <div className="text-gray-600 text-lg">電子週報</div>
+          <div className="text-gray-600 text-lg">焦點議題</div>
         </div>
       </header>
+
       <main className="bg-blue-300 w-full min-h-screen flex">
         {/* left */}
         <div className="w-[40%] flex">
           {/* 理財電子週報 */}
           <div className="text-white w-[50%] h-full p-2 flex flex-col justify-around">
-            <div className="text-2xl font-bold">理財電子週報</div>
+            <div className="text-2xl font-bold shadow-lg">理財電子週報</div>
             <div className="text-xl font-semibold">
               偏高之美債殖利率市場震盪
               <br />
@@ -38,29 +43,31 @@ const Index = () => {
               <div className="flex justify-around items-center w-full">
                 <div className="w-[10%] h-[30px] bg-white"></div>
                 <div className="w-[80%] text-black">
-                  ddddddddddddddddddddddddddddd
-                  <div className="text-gray-500">ddddddddddddddddddddddddddddd</div>
+                  金融占台股市值比僅次半導體 GDP貢獻12年來近零成長
+                  <div className="text-gray-500">2023年11月26日</div>
                 </div>
               </div>
               <div className="flex justify-around items-center w-full">
                 <div className="w-[10%] h-[30px] bg-white"></div>
                 <div className="w-[80%] text-black">
-                ddddddddddddddddddddddddddddd
-                  <div className="text-gray-500">ddddddddddddddddddddddddddddd</div>
+                  台積電賺很多股價「要死不活」 反觀死敵噴漲30％ 驚爆2大弱點
+                  <div className="text-gray-500">2023年11月24日</div>
                 </div>
               </div>
               <div className="flex justify-around items-center w-full">
                 <div className="w-[10%] h-[30px] bg-white"></div>
                 <div className="w-[80%] text-black">
-                ddddddddddddddddddddddddddddd
-                  <div className="text-gray-500">ddddddddddddddddddddddddddddd</div>
+                  Nvidia beats TSMC and Intel to take top chip industry revenue
+                  crown for the first time
+                  <div className="text-gray-500">2023年11月24日</div>
                 </div>
               </div>
               <div className="flex justify-around items-center w-full">
                 <div className="w-[10%] h-[30px] bg-white"></div>
                 <div className="w-[80%] text-black">
-                ddddddddddddddddddddddddddddd
-                  <div className="text-gray-500">ddddddddddddddddddddddddddddd</div>
+                  TSMC (TSM) Stock Declines While Market Improves: Some
+                  Information for Investors
+                  <div className="text-gray-500">2023年11月25日</div>
                 </div>
               </div>
             </div>
@@ -70,29 +77,29 @@ const Index = () => {
               <div className="flex justify-around items-center w-full">
                 <div className="w-[10%] h-[30px] bg-white"></div>
                 <div className="w-[80%] text-black">
-                ddddddddddddddddddddddddddddd
-                  <div className="text-gray-500">ddddddddddddddddddddddddddddd</div>
+                  快刪掉！17款「安卓APP」會偷錢 超過1200萬人下載
+                  <div className="text-gray-500">2023年12月10日</div>
                 </div>
               </div>
               <div className="flex justify-around items-center w-full">
                 <div className="w-[10%] h-[30px] bg-white"></div>
                 <div className="w-[80%] text-black">
-                ddddddddddddddddddddddddddddd
-                  <div className="text-gray-500">ddddddddddddddddddddddddddddd</div>
+                  快更新！一鍵優化Google Pixel 8遊戲效能 玩《原神》更順了
+                  <div className="text-gray-500">2023年12月10日</div>
                 </div>
               </div>
               <div className="flex justify-around items-center w-full">
                 <div className="w-[10%] h-[30px] bg-white"></div>
                 <div className="w-[80%] text-black">
-                ddddddddddddddddddddddddddddd
-                  <div className="text-gray-500">ddddddddddddddddddddddddddddd</div>
+                  高盛打臉降息! ? SOFR訊號亦透漏短線債券超漲| Anue鉅亨- 專家觀點
+                  <div className="text-gray-500">2023年12月10日</div>
                 </div>
               </div>
               <div className="flex justify-around items-center w-full">
                 <div className="w-[10%] h-[30px] bg-white"></div>
                 <div className="w-[80%] text-black">
-                ddddddddddddddddddddddddddddd
-                  <div className="text-gray-500">ddddddddddddddddddddddddddddd</div>
+                  年底買車注意「年份、年式」 德系車1年恐差「這金額」
+                  <div className="text-gray-500">2023年12月10日</div>
                 </div>
               </div>
             </div>
@@ -106,8 +113,11 @@ const Index = () => {
               投資數據儀表板
             </div>
             <div className="w-full text-base flex justify-end">
-                <input placeholder="search" className="max-w-[250px] rounded-md p-1"></input>
-              </div>
+              <input
+                placeholder="search"
+                className="max-w-[250px] rounded-md p-1"
+              ></input>
+            </div>
             <div className="w-full flex">
               {/* left */}
               <div className="w-[50%] p-1 mt-5">
@@ -145,65 +155,84 @@ const Index = () => {
               </div>
               {/* right */}
               <div className="w-[50%] p-1 flex flex-col items-center justify-around mt-5">
-                
                 <div className="text-2xl text-green-600 font-bold">
                   其他投資機會
                 </div>
 
                 <div className="flex flex-col justify-between h-[45%]">
                   <div className="flex items-center justify-between ">
-                    <div className="w-[10%] h-[30px] bg-white"></div>
+                    <img
+                      src="sun-emoji.png"
+                      className="ml-4 w-[50px] h-[50px]"
+                    ></img>
                     <div className="w-[80%] text-black">
-                    ddddddddddddddddddddddddddddd
-                      <div className="text-gray-500">ddddddddddddddddddddddddddddd</div>
+                      遠傳Q3法說會／前三季EPS追平2020全年
+                      <div className="text-gray-500">
+                        公布第三季合併總營收新台幣232.2億元，前三季每股盈餘達到2.56元，追平...
+                      </div>
                     </div>
                   </div>
 
                   <div className="flex items-center justify-between">
-                    <div className="w-[10%] h-[30px] bg-white"></div>
+                    <img
+                      src="partly-cloudy-day--v1.png"
+                      className="ml-4 w-[50px] h-[50px]"
+                    ></img>
                     <div className="w-[80%] text-black">
-                    ddddddddddddddddddddddddddddd
-                      <div className="text-gray-500">ddddddddddddddddddddddddddddd</div>
+                      玉山金法說會／各項財業務表現良好 前三季獲利超去年全年
+                      <div className="text-gray-500">
+                        2023年前9月金控稅後純益158.9億元，獲利已超過去年全年，玉山金...
+                      </div>
                     </div>
                   </div>
 
                   <div className="flex items-center justify-between">
-                    <div className="w-[10%] h-[30px] bg-white"></div>
+                    <img
+                      src="external-rainny-spring-filled-outline-lima-studio.png"
+                      className="ml-4 w-[50px] h-[50px]"
+                    ></img>
                     <div className="w-[80%] text-black">
-                    ddddddddddddddddddddddddddddd
-                      <div className="text-gray-500">ddddddddddddddddddddddddddddd</div>
+                      中光電法說會／今年節能、影像產品出貨各約年減2-3成
+                      <div className="text-gray-500">
+                        中光電（5371）總經理林惠姿今（31）日表示，因電視及公眾顯示器（PID）...
+                      </div>
                     </div>
                   </div>
 
                   <div className="flex items-center justify-between">
-                    <div className="w-[10%] h-[30px] bg-white"></div>
+                    <img
+                      src="sun-emoji.png"
+                      className="ml-4 w-[50px] h-[50px]"
+                    ></img>
                     <div className="w-[80%] text-black">
-                    ddddddddddddddddddddddddddddd
-                      <div className="text-gray-500">ddddddddddddddddddddddddddddd </div>
+                      中鼎法說會／在建工程達3,537億元 創歷史新高
+                      <div className="text-gray-500">
+                        中鼎第3季稅後純益4.44億元，年增28.2％，每股稅後純益0.56元，優...{" "}
+                      </div>
                     </div>
                   </div>
                 </div>
 
                 <div className="flex w-full justify-end">
-                  <div className="flex">
-                    <div className="w-[25px] h-[25px] bg-white"></div>
+                  <div className="flex items-center">
+                    <img src="sun-emoji.png"></img>
                     正向
                   </div>
-                  <div className="flex ml-2">
-                    <div className="w-[25px] h-[25px] bg-white"></div>
+                  <div className="flex items-center">
+                    <img src="sun-emoji.png"></img>
                     中立
                   </div>
-                  <div className="flex ml-2">
-                    <div className="w-[25px] h-[25px] bg-white"></div>
+                  <div className="flex items-center">
+                    <img src="sun-emoji.png"></img>
                     保守
                   </div>
                 </div>
 
                 <div className="w-full flex justify-end">
-                  <button className="text-2xl text-white bg-green-600 py-3 px-5 rounded-lg">了解更多</button>
+                  <button className="text-2xl text-white bg-green-600 py-3 px-5 rounded-lg">
+                    了解更多
+                  </button>
                 </div>
-                
-
               </div>
             </div>
           </div>
